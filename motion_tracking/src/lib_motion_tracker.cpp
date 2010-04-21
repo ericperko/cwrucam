ec6240ba6deecd16d816f06db5045f91de6f76b0
Chad Rockey <chad@theprince.(none)>
2010-04-20 23:52:31 -0400

    Added start of color thresholding, will hopefully find sponge.

--- motion_tracking/src/lib_motion_tracker.cpp
+++ motion_tracking/src/lib_motion_tracker.cpp
@@ -11,12 +11,27 @@ int ghigh = 120;
 int blow = 70;
 int bhigh = 100;
 
+using namespace cv;
 void blobfind(const cv::Mat& src, cv::Mat& out)
 {
-	cv::vector<cv::Mat> mats;
+	vector<cv::Mat> mats;
 
-	cv::split(src, mats);
+	split(src, mats);
+
+	// Set all values below value to zero, leave rest the same
+	// Then inverse binary threshold the remaining pixels
 	
-	// Show green channel
-	out = mats[1];
+	// Threshold blue channel
+	threshold(mats[0], mats[0], blow, 255, 3);
+	threshold(mats[0], mats[0], bhigh, 255, 1);
+	// Threshold green channel
+	threshold(mats[1], mats[1], glow, 255, 3);
+	threshold(mats[1], mats[1], ghigh, 255, 1);
+	// Threshold red channel
+	threshold(mats[2], mats[2], rlow, 255, 3);
+	threshold(mats[2], mats[2], rhigh, 255, 1);
+
+	// Multiply binary images to produce location of sponge
+	multiply(mats[0],mats[1],out);
+	multiply(mats[2],out,out);
 }
